a98df4a06edb947d5ac08ec248a6f15402281ce9
MaksymMatviichuk13666 <maksymmatviichuk-lhk@team.asana.com>
2025-01-12 16:54:40 +0200

    top creators layout fixes

--- src/components/shared/TopCreatorCard.tsx
+++ src/components/shared/TopCreatorCard.tsx
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const TopCreatorCard = ({ creator }: any) => {
     return (
-        <div className="flex flex-center flex-col gap-4 p-5 max-w-[190px] w-[190px] rounded-[20px] border border-[#1F1F22] break-all">
-            <img
-                src={creator.imageUrl}
-                className='rounded-full'
-                alt=""
-                width={64}
-                height={64}
-            />
-            <p className=''>{creator.name}</p>
+        <>
             <Link
-                className='shad-button_primary whitespace-nowrap pt-1 pb-1 pr-4 pl-4 rounded-[5px]'
+                className='flex flex-center flex-col gap-4 p-5 max-w-[190px] w-[190px] rounded-[20px] border border-[#1F1F22] break-all'
                 to={`profile/${creator.$id}`}
             >
-                Profile
+                <img
+                    src={creator.imageUrl}
+                    className='rounded-full'
+                    alt=""
+                    width={64}
+                    height={64}
+                />
+                <p>{creator.name}</p>
             </Link>
-        </div>
+        </>
     )
 }
 
